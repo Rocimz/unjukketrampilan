6fe8b9cf42c78b393c6c24a9a8e2ae170f3df665
Rocimz <nrocimz@gmail.com>
2024-04-07 14:34:34 +0700

    membuat model dan migration

---
+++ app/Models/post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class post extends Model
+{
+    use HasFactory;
+    protected $table='post';
+    protected $guarded='[id]';
+
+    function produk()  {
+        return $this->belongsTo(produk::class);
+    }
+}
